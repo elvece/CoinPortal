ad886cb44435fd15bb820a83223bcb50ca277674
elvece <lvcifferello@gmail.com>
2017-08-08 20:00:37 -0600

    added react to client side, updated server start

---
+++ src/client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>CoinPortal</h2>
+        </div>
+        <p className="App-intro">
+          Exchange Table
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
