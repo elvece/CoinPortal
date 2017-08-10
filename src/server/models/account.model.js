a214c7b80dde18859925c31e7e1b70ec1160babb
elvece <lvcifferello@gmail.com>
2017-08-09 20:26:48 -0600

    set up account model

---
+++ src/server/models/account.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+
+const AccountSchema = new mongoose.Schema({
+  id: Schema.Types.ObjectId,
+  username: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    required: true
+  },
+  name: String,
+  balances: Schema.Types.Mixed,
+  wallets: Schema.Types.Mixed,
+  transactions: Schema.Types.Mixed
+});
+
+// statics exist directly on model
+AccountSchema.statics = {
+  get(id){
+    return this.findById(id)
+      .exec()
+      .then((account) => {
+        if (account){
+          return account;
+        }
+        return Promise.reject('No account found.')
+      });
+  },
+  list(){// returns all accounts
+    return this.find()
+      .sort({})
+  }
+}
+
+// methods can be called on any instance of a model
+AccountSchema.methods.getBalance = function(coinType){
+  return this.balances[coinType];
+}
+
+AccountSchema.methods.getWallet = function(coinType){
+  return this.wallets[coinType];
+}
+
+module.exports = {
+  mongoose.model('Accounts', AccountSchema);
+};
