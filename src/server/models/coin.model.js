9c6de0f97e74d91be65b5984144ac26d1189b914
elvece <lvcifferello@gmail.com>
2017-08-19 14:53:07 -0600

    updated coin model to include miner fee

--- src/server/models/coin.model.js
+++ src/server/models/coin.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const CoinSchema = new mongoose.Schema({
   name: String,
   url: String,
-  price: String // String rather than Number so can accept decimal places
+  price: String, // String rather than Number so can accept decimal places
+  minerFee: Number
 });
 
 module.exports = mongoose.model('Coins', CoinSchema);
