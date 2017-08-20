bb24b907c1e5ca0895dc0edeb7c29a7858b07802
elvece <lvcifferello@gmail.com>
2017-08-20 16:22:57 -0600

    added icon package, styled header, set up table grid

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,35 +1,54 @@
 import React, { Component } from 'react';
-import Client from "./Client";
 import './App.css';
 import ExchangeForm from './components/ExchangeForm.js';
 import ExchangeTable from './components/ExchangeTable.js';
 import AccountManager from './components/AccountManager.js';
+import { MdAllOut } from 'react-icons/lib/md';
+
 
 class App extends Component {
+
   /* TODO:
       (1) make ExchangeManager to handle getting/setting exchange data
       (2) pass data into ExchangeForm and ExchangeTable
       (3) refactor ExchangeForm and Exchange Table to handle data as props
   */
 
-  getData = () => {
-    Client.getStuff(`api/exchanges/`).then(console.log('hi'));
-  };
-
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>CoinPortal</h2>
+    <div>
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header className="mdl-layout__header">
+          <div className="mdl-layout__header-row">
+            <MdAllOut className="App-header-icon"/>
+            <span className="mdl-layout-title">CoinPortal</span>
+            <div className="mdl-layout-spacer"></div>
+            <nav className="mdl-navigation mdl-layout--large-screen-only">
+              <a className="mdl-navigation__link" href="">Dashboard</a>
+              <a className="mdl-navigation__link" href="">Table</a>
+            </nav>
+          </div>
+        </header>
+        <div className="mdl-layout__drawer">
+          <span className="mdl-layout-title">Title</span>
+          <nav className="mdl-navigation">
+            <a className="mdl-navigation__link" href="">Link</a>
+            <a className="mdl-navigation__link" href="">Link</a>
+          </nav>
         </div>
-        <p className="App-intro">
-          Exchange Table
-        </p>
-        <button onClick={() => this.getData()}>Click me</button>
-        <ExchangeForm/>
-        <ExchangeTable/>
-        <AccountManager/>
+        <main className="mdl-layout__content">
+          <div className="page-content">
+            <div className="mdl-grid">
+              <div className="mdl-cell mdl-cell--8-col mdl-cell--2-offset">
+                <ExchangeTable/>
+              </div>
+            </div>
+            <AccountManager/>
+            <ExchangeForm/>
+          </div>
+        </main>
       </div>
+    </div>
     );
   }
 }
