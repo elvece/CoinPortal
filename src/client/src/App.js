79c189a8067aac1bd38e898df7cc634b8fa81337
elvece <lvcifferello@gmail.com>
2017-08-22 10:15:08 -0600

    setting mui theme, mainly for changing stepper color

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    //primary1Color: '#224D7F',//dark cerulean
+    primary1Color: '#892323',//vivid auburn for now
+    primary2Color: '#1C6D65',//deep green cyan turquoise
+    primary3Color: '#222',//faded black
+    accent1Color: '#E2B761',//earth yellow
+    accent2Color: '#892323',//vivid auburn
+    accent3Color: '#A3A19C',//quick silver
+    alternateTextColor: '#F4F4F2'
+  }
+});
 
 class App extends Component {
 
   render() {
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
         <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
           <Header/>
           <Main/>
