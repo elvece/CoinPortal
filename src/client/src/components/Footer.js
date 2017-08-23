410d244cf38032b8e396f466f8f5a1e772430d2a
elvece <lvcifferello@gmail.com>
2017-08-23 12:52:59 -0600

    updating env for heroku mongodb and preventing target blank noopener

--- src/client/src/components/Footer.js
+++ src/client/src/components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
     return (
       <footer>
         <div style={{textAlign: 'center'}}>
-          <a href="https://github.com/elvece/coin-portal" target="_blank"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwjaQPPek4x-S_r1rdmprzPzGMbmdQ7tX1yBvjbXrKjYQ5bpSA98i9u8c" alt="GitHub" height="50"/></a>
+          <a href="https://github.com/elvece/coin-portal" target="_blank" rel="noopener noreferrer"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwjaQPPek4x-S_r1rdmprzPzGMbmdQ7tX1yBvjbXrKjYQ5bpSA98i9u8c" alt="GitHub" height="50"/></a>
         </div>
       </footer>
     );
