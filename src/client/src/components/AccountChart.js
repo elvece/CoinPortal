dd8405f94be7b0ef0a848b444f6aa8974574e897
elvece <lvcifferello@gmail.com>
2017-08-23 01:10:54 -0600

    calculations for account chart gain/loss working

--- src/client/src/components/AccountChart.js
+++ src/client/src/components/AccountChart.js
@@ -1,46 +1,94 @@
 import React, { Component } from 'react';
 import '../App.css';
-import {Doughnut} from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 
 class AccountChart extends Component {
 
   render(){
     const account = this.props.account;
-    const prop_wallets = this.props.account.wallets;
-    const wallets = prop_wallets && prop_wallets.length > 0 ? prop_wallets.map((wallet, i) => {
-      return (
-        <p key={i}>{wallet.coin}: {wallet.address}</p>
-      )
-    }) : []
-    const data = {
-      labels: [
-        'Red',
-        'Green',
-        'Yellow'
-      ],
-      datasets: [{
-        data: [300, 50, 100],
-        backgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
+    const prop_wallets = this.props.account.wallets ? this.props.account.wallets.map((wallet) => {
+      let totalCoinAmount = 0;
+      let totalOgValue = 0;
+      // NOTE using for each for readability, when scale, use for loop for performance
+      wallet.rates.forEach((tx) => {
+        totalCoinAmount += tx.amount;// add all transactions amounts together for total coin amount
+        totalOgValue += (tx.rate * tx.amount)// multiply to get the original purchase value
+      })
+      return {
+        coin: wallet.coin,
+        totalCoinAmount: totalCoinAmount,
+        totalOgValue: totalOgValue
+      };
+    }) : [];
+
+    //temp values TODO connect with coincap price for each
+    const btcPrice = 4100;
+    const dashPrice = 290;
+    const ethPrice = 315;
+    const ltcPrice = 46;
+    const chartData = calculcateDataset() ? calculcateDataset() : {};
+
+
+    function calculcateDataset(){
+      let chartData = {
+        labels: [
+          'Gains',
+          'Losses',
+          'Original Investment'
         ],
-        hoverBackgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
-        ]
-      }]
-    };
+        datasets: []
+      };
+
+      prop_wallets.forEach((wallet) => {
+        let currentValue = 0;
+        let diff = 0;
+        let dataObj = {
+          data: [],//0 = gains, 1 = losses, 2 = current value of holding - for chart colors
+          backgroundColor: [
+            '#1C6D65',
+            '#E2B761',
+            '#224D7F'
+          ],
+          hoverBackgroundColor: [
+            '#258E84',
+            '#FFCE6D',
+            '#3D6EA5'
+          ]
+        }
+        //calculate current value based on current coin price
+        if(wallet.coin === 'BTC'){
+          currentValue = wallet.totalCoinAmount * btcPrice;
+        } else if(wallet.coin === 'ETH'){
+          currentValue = wallet.totalCoinAmount * ethPrice;
+        } else if(wallet.coin === 'LTC'){
+          currentValue = wallet.totalCoinAmount * ltcPrice;
+        } else if(wallet.coin === 'DASH'){
+          currentValue = wallet.totalCoinAmount * dashPrice;
+        }
+        //set final data value to total original investments
+        dataObj.data[2] = Math.round(wallet.totalOgValue * 1e2) / 1e2;
+        //calculate gain or loss of investment
+        diff = currentValue - wallet.totalOgValue;
+
+        if(diff < 0){//loss
+          dataObj.data[1] = Math.round(diff * 1e2) / 1e2;
+          dataObj.data[0] = 0;
+        } else {//gain
+          dataObj.data[0] = Math.round(diff * 1e2) / 1e2;
+          dataObj.data[1] = 0;
+        }
+        chartData.datasets.push(dataObj);
+      })
+      return chartData;
+    }
 
     return (
       <div>
         <p>{account.name}</p>
         <p>{account.username}</p>
-        {wallets}
         <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" onClick={this.props.onClick}>Update</button>
         <div>
-        <Doughnut data={data} />
+        <Doughnut data={chartData}/>
         </div>
       </div>
     );
