1d522a2701dfa208f5e715e1022adfb4082138ec
elvece <lvcifferello@gmail.com>
2017-08-23 07:05:12 -0600

    created routes for coincap data

---
+++ src/server/routes/coin.route.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const request = require('request-promise');
+
+// GET /api/coins/prices - gets list of all coin prices (and other trend data)
+router.get('/prices', function(req, res, next) {
+  const options = {
+    uri: 'https://api.coinmarketcap.com/v1/ticker/',
+    json: true
+  };
+  request(options)
+    .then(function(result){
+      res.send(result);
+    })
+    .catch(function(e){
+      console.log(' ****** get  /api/coins/price ERROR: ', e)
+      next(e);
+    });
+});
+// GET /api/coins/prices/:coin - gets coin price and trend data
+router.get('/price/:coin', function(req, res, next) {
+  const options = {
+    uri: 'https://api.coinmarketcap.com/v1/ticker/'+req.params.coin,
+    json: true
+  };
+  request(options)
+    .then(function(result){
+      res.send(result);
+    })
+    .catch(function(e){
+      console.log(' ****** get /api/coins/prices/:coin ERROR: ', e)
+      next(e);
+    });
+});
+// GET /api/coins/icons - gets icon information for all coins
+router.get('/icons', function(req, res, next) {
+  const options = {
+    uri: 'https://shapeshift.io/getcoins/',
+    json: true
+  };
+  request(options)
+    .then(function(result){
+      res.send(result);
+    })
+    .catch(function(e){
+      console.log(' ****** get /api/coins/icons ERROR: ', e)
+      next(e);
+    });
+});
+
+
+module.exports = router;
--- src/server/routes/index.route.js
+++ src/server/routes/index.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const exchangeRoutes = require('./exchange.route');
 const accountRoutes = require('./account.route');
+const coinRoutes = require('./coin.route');
 
 // GET /sanity-check - checks service working
 router.get('/sanity-check', function (req, res, next) {
@@ -12,5 +13,7 @@ router.get('/sanity-check', function (req, res, next) {
 router.use('/api/exchanges', exchangeRoutes);
 // mount account routes at /api/accounts
 router.use('/api/accounts', accountRoutes);
+// mount coin routes at /api/coins
+router.use('/api/coins', coinRoutes);
 
 module.exports = router;
