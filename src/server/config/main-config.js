3730bcdc1589d337018f1d6b06f628a91a9b933d
elvece <lvcifferello@gmail.com>
2017-08-24 18:44:26 -0600

    trying again to serve route handler

--- src/server/config/main-config.js
+++ src/server/config/main-config.js
@@ -42,14 +42,14 @@
     //   saveUninitialized: true
     // }));
     app.use(flash());
-    app.use(express.static(path.join(__dirname, '..', '..', 'client')));
+    // app.use(express.static(path.join(__dirname, '..', '..', 'client')));
 
     // Serve static files from the React app
     app.use(express.static(path.join(__dirname, '..', '..','client/build')));
     //catch-all to fall back on react build root
-    // app.get('*', (req, res) => {
-    //   res.sendFile(path.join(__dirname, '..', '..','/client/build/index.html'));
-    // })
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '..', '..','/client/build/index.html'));
+    })
 
   };
 
