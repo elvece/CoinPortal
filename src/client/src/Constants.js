48034bc2d0805151f6e4b601d3905c015e5f5dc6
elvece <lvcifferello@gmail.com>
2017-08-29 09:09:59 -0600

    added constants file

---
+++ src/client/src/Constants.js
@@ -0,0 +1,12 @@
+const constants = {
+  BTC: 'BTC',
+  ETH: 'ETH',
+  DASH: 'DASH',
+  LTC: 'LTC',
+  SHAPESHIFT: 'ShapeShift',
+  POLONIEX: 'Poloniex',
+  DARK_RED: '#892323',
+  SMOKE_WHITE: '#F4F4F2'
+}
+
+export constants;
